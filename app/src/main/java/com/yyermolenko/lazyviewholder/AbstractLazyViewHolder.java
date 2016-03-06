fbc96b2a0cb8c0fb10bb4ad548d37cefe2f58511
Yarsolav Yermolenko <yyermolenko@gmail.com>
2016-03-06 15:53:12 +0200

    Cast to popular view classes

--- app/src/main/java/com/yyermolenko/lazyviewholder/AbstractLazyViewHolder.java
+++ app/src/main/java/com/yyermolenko/lazyviewholder/AbstractLazyViewHolder.java
@@ -3,11 +3,16 @@ package com.yyermolenko.lazyviewholder;
 import android.app.Activity;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public abstract class AbstractLazyViewHolder implements LazyHolder {
 
     protected SparseArray<View> holder = new SparseArray<>();
 
+    @Override
     public View find(View view, int id) {
         View foundView = view.findViewById(id);
         if (foundView != null)
@@ -16,12 +21,33 @@ public abstract class AbstractLazyViewHolder implements LazyHolder {
         return foundView;
     }
 
+    @Override
     public View find(Activity activity, int id) {
         View foundView = activity.findViewById(id);
         if (foundView != null)
             holder.put(id, foundView);
 
         return foundView;
+    }
+
+    @Override
+    public TextView findTextView(int id) {
+        return (TextView) find(id);
+    }
+
+    @Override
+    public Button findButton(int id) {
+        return (Button) find(id);
+    }
+
+    @Override
+    public ImageView findImageView(int id) {
+        return (ImageView) find(id);
+    }
+
+    @Override
+    public ViewGroup findViewGroup(int id) {
+        return (ViewGroup) find(id);
     }
 
 }
--- app/src/main/java/com/yyermolenko/lazyviewholder/LazyHolder.java
+++ app/src/main/java/com/yyermolenko/lazyviewholder/LazyHolder.java
@@ -2,6 +2,10 @@ package com.yyermolenko.lazyviewholder;
 
 import android.app.Activity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public interface LazyHolder {
 
@@ -10,5 +14,13 @@ public interface LazyHolder {
     View find(View view, int id);
 
     View find(Activity activity, int id);
+
+    TextView findTextView(int id);
+
+    Button findButton(int id);
+
+    ImageView findImageView(int id);
+
+    ViewGroup findViewGroup(int id);
 
 }
